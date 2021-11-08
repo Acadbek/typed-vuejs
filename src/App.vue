562be57bd52a9eadbd52ebc1ab4c721a46041ab4
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2021-11-08 16:02:06 +0500

    feat: typed

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <Typedd/>
+</template>
+
+<script>
+import Typedd from './components/Typedd.vue'
+export default {
+  name: 'App',
+  components: {
+    Typedd
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Typedd.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="typed">
+    <vue-typed-js
+      class="typeddd"
+      :strings="['Asadbek Nosirjonov', 'Front-end']"
+      :typeSpeed="90"
+      :startDelay="1000"
+      :backSpeed="20"
+      :smartBackspace="true"
+      :loop="true"
+    >
+      <h1 class="typing"></h1>
+    </vue-typed-js>
+  </div>
+</template>
+
+<script>
+Vue.use(VueTypedJs);
+import Vue from "vue";
+import { VueTypedJs } from "vue-typed-js";
+export default {
+  components: {
+    VueTypedJs,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.typed {
+  width: 100vw;
+  height: 100vw;
+  background-image: url("../assets/Texture-wallpaper-HD-download-desktop.jpg");
+  background-repeat: no-repeat;
+}
+.typing {
+  text-align: center;
+  padding-top: 20%;
+  color: #fff;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-weight: 700;
+  font-size: 30px;
+}
+.typeddd {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
